5c6d33ad7683b7b042f2aa129fa1c69f954e30de
maynard.igot <maynard.igot@PCA109.JPI.Local>
2021-02-02 11:30:49 +0800

    My first Eclipse project commit

---
+++ Logging/test/org/fujitsu/training/codes/TestMathematics.java
@@ -0,0 +1,37 @@
+package org.fujitsu.training.codes;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestMathematics {
+
+	private Mathematics math;
+	
+	@BeforeEach
+	public void setup() {
+		math = new Mathematics();
+	}
+	
+	@AfterEach
+	public void teardown() {
+		math = null;
+	}
+	
+	@Test
+	public void testSum01() {
+		math.add(10, 10);
+	}
+	
+	@Test
+	public void testDiv01() {
+		try {
+			math.div(10, 0);
+		} catch (ArithmeticException e) {
+			Assertions.fail(e.getMessage());
+		} catch (Exception e) {
+			Assertions.fail(e.getMessage());
+		}
+	}
+}
